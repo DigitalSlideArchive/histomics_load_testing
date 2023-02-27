329705f23ae3f1b3c1318bd7c710a8805a5fe2c1
Anne Haley <anne.haley@kitware.com>
2023-02-27 11:23:00 -0500

    Start populate server script

---
+++ README.md
@@ -0,0 +1,13 @@
+## This repository is a testing suite for HistomicsUI Tile Serving.
+See https://github.com/DigitalSlideArchive/HistomicsUI.
+
+
+------
+
+### Getting Started
+
+ 1. Run `docker-compose up` in the top-level directory to start HistomicsUI
+
+ 2. Edit `login.cfg` with your preferred credentials
+
+ 3. Run `python3 populate_server.py` to initialize your user and create the assetstore, collection, and example image item for testing
---
+++ populate_server.py
@@ -0,0 +1,99 @@
+# Please ensure the server is active before running this script
+# references https://github.com/DigitalSlideArchive/digital_slide_archive/blob/master/devops/dsa/provision.py
+import configparser
+import os
+from pathlib import Path
+
+from girder.models.assetstore import Assetstore
+from girder.models.collection import Collection
+from girder.models.folder import Folder
+from girder.models.item import Item
+from girder.models.upload import Upload
+from girder.models.user import User
+from girder_large_image.models.image_item import ImageItem
+
+
+config = configparser.ConfigParser()
+config.read("../login.cfg")
+
+server_url = "http://localhost:8080/api/v1"
+
+
+def create_admin_user():
+    # If there is are no admin users, create an admin user
+    if User().findOne({"admin": True}) is None:
+        adminParams = {
+            "login": config["LOGIN"]["USERNAME"],
+            "password": config["LOGIN"]["PASSWORD"],
+            "firstName": "Admin",
+            "lastName": "Admin",
+            "email": "admin@nowhere.nil",
+            "public": True,
+        }
+        User().createUser(admin=True, **adminParams)
+    adminUser = User().findOne({"admin": True})
+    return adminUser
+
+
+def create_assetstore():
+    # Make sure we have an assetstore
+    root = Path(".").absolute() / "../assetstore"
+    if Assetstore().findOne() is None:
+        Assetstore().createFilesystemAssetstore(name="Root", root=root)
+
+
+def create_collection_folder(adminUser, collName, folderName):
+    if Collection().findOne({"lowerName": collName.lower()}) is None:
+        Collection().createCollection(collName, adminUser)
+    collection = Collection().findOne({"lowerName": collName.lower()})
+    if (
+        Folder().findOne(
+            {"parentId": collection["_id"], "lowerName": folderName.lower()}
+        )
+        is None
+    ):
+        Folder().createFolder(
+            collection,
+            folderName,
+            parentType="collection",
+            public=True,
+            creator=adminUser,
+        )
+    folder = Folder().findOne(
+        {"parentId": collection["_id"], "lowerName": folderName.lower()}
+    )
+    return folder
+
+
+def upload_example(folder, adminUser):
+    exampleFile = {"name": "example.tiff", "path": "data/example.tiff"}
+    item = item = Item().findOne(
+        {"folderId": folder["_id"], "name": exampleFile["name"]}
+    )
+    if not item:
+        item = Item().createItem(exampleFile["name"], creator=adminUser, folder=folder)
+        with open(Path(exampleFile["path"]), "rb") as f:
+            Upload().uploadFromFile(
+                f,
+                os.path.getsize(exampleFile["path"]),
+                name=exampleFile["name"],
+                parentType="item",
+                parent=item,
+                user=adminUser,
+            )
+            try:
+                ImageItem().createImageItem(
+                    item,
+                    {"itemId": item["_id"]},
+                )
+            except Exception as e:
+                print(e)
+    print(item)
+    return item
+
+
+if __name__ == "__main__":
+    adminUser = create_admin_user()
+    create_assetstore()
+    folder = create_collection_folder(adminUser, "Examples", "Data")
+    upload_example(folder, adminUser)
